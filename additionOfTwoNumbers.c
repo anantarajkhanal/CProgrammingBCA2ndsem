5929cd043580ebb95b6c03cc3025762e29499652
anantarajkhanal <ananta5441@gmail.com>
2024-06-27 10:57:28 +0545

    updated by Ananta khanal on june 27 at 11:00am

---
+++ additionOfTwoNumbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int firstnumber , secondnumber , sum;
+printf("enter firstnumber and second number\n");
+scanf("%d %d",&firstnumber,&secondnumber);
+
+sum = firstnumber+secondnumber;
+printf("%d is the sum of both numbers",sum);
+return 0;
+}
